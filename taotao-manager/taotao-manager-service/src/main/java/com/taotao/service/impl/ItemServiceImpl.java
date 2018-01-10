62d545766c9b2c784d4755089a3e2bd2bf856c54
songxiaojun <1215817408@qq.com>
2018-01-10 23:25:28 +0800

    根据id查询

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/ItemServiceImpl.java
@@ -0,0 +1,29 @@
+package com.taotao.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.mapper.TbItemMapper;
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+
+/**
+ * 商品管理Service
+ * <p>Title: ItemServiceImpl</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.cn</p> 
+ * @version 1.0
+ */
+@Service
+public class ItemServiceImpl implements ItemService {
+
+	@Autowired
+	private TbItemMapper itemMapper;
+	
+	@Override
+	public TbItem getItemById(long itemId) {
+		TbItem item = itemMapper.selectByPrimaryKey(itemId);
+		return item;
+	}
+	
+}

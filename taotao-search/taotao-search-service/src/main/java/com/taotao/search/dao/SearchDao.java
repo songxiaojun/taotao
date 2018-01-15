2b5501ae7e785dbfa0c29224e8b870181c40b00e
songxiaojun <1215817408@qq.com>
2018-01-15 22:36:39 +0800

    商品导入索引库

---
+++ taotao-search/taotao-search-service/src/main/java/com/taotao/search/dao/SearchDao.java
@@ -0,0 +1,67 @@
+package com.taotao.search.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrDocumentList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.taotao.common.pojo.SearchItem;
+import com.taotao.common.pojo.SearchResult;
+
+/**
+ * 查询索引库商品dao
+ * <p>Title: SearchDao</p>
+ * <p>Description: </p>
+ * <p>Company: www.itcast.cn</p> 
+ * @version 1.0
+ */
+@Repository
+public class SearchDao {
+	
+	@Autowired
+	private SolrServer solrServer;
+
+	public SearchResult search(SolrQuery query) throws Exception{
+		//根据query对象进行查询
+		QueryResponse response = solrServer.query(query);
+		//取查询结果
+		SolrDocumentList solrDocumentList = response.getResults();
+		//取查询结果总记录数
+		long numFound = solrDocumentList.getNumFound();
+		SearchResult result = new SearchResult();
+		result.setRecordCount(numFound);
+		List<SearchItem> itemList = new ArrayList<>();
+		//把查询结果封装到SearchItem对象中
+		for (SolrDocument solrDocument : solrDocumentList) {
+			SearchItem item = new SearchItem();
+			item.setCategory_name((String) solrDocument.get("item_category_name"));
+			item.setId((String) solrDocument.get("id"));
+			item.setImage((String) solrDocument.get("item_image"));
+			item.setPrice((long) solrDocument.get("item_price"));
+			item.setSell_point((String) solrDocument.get("item_sell_point"));
+			//取高亮显示
+			Map<String, Map<String, List<String>>> highlighting = response.getHighlighting();
+			List<String> list = highlighting.get(solrDocument.get("id")).get("item_title");
+			String title = "";
+			if (list != null && list.size() > 0) {
+				title = list.get(0);
+			} else {
+				title = (String) solrDocument.get("item_title");
+			}
+			item.setTitle(title);
+			//添加到商品列表
+			itemList.add(item);
+		}
+		//把结果添加到SearchResult中
+		result.setItemList(itemList);
+		//返回
+		return result;
+	}
+}
